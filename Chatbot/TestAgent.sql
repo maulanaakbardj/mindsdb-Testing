7dbafa9b583d5f7837e510be2466cdfa9fa2cafc
Maulana Akbar <67249292+maulanaakbardj@users.noreply.github.com>
2023-12-25 18:41:17 +0700

    Create TestAgent.sql

---
+++ Chatbot/TestAgent.sql
@@ -0,0 +1,10 @@
+--- Public demo database
+CREATE DATABASE mysql_demo_db
+WITH ENGINE = "mysql",
+PARAMETERS = {
+   "user": "user",
+   "password": "MindsDBUser123!",
+   "host": "db-demo-data.cwoyhfn6bzs0.us-east-1.rds.amazonaws.com",
+   "port": "3306",
+   "database": "public"
+   };
